fff1803321f5914fc0b2e11dc1474f97f43946e6
ioana maniga <ioana.mna@gmail.com>
2020-03-30 02:29:33 +0300

    added simptoms tab but without conflict functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,12 @@
-import {Component} from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
 
-/*
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
-*/
+import {Component, OnInit} from '@angular/core';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-
 
 
 
