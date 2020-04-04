5c5f54f7c6950364464dd67456f8e49713fbff4b
andreear7 <61412181+andreear7@users.noreply.github.com>
2020-04-04 20:02:01 +0300

    actually added steppers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 
 import {Component, OnInit} from '@angular/core';
-
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,22 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class AppComponent {
+  isLinear =true;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup:FormGroup;
 
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.thirdFormGroup = this._formBuilder.group({
+      thirdCtrl: ['', Validators.required]
+    });
+  }
 
 }
 
