48e05a62b40f21d83ea7d03fdbb71acaa6eef6b4
Ionuut08 <ionut.feraru@info.uaic.ro>
2020-04-10 23:29:13 +0300

    incomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators, FormControl, ValidationErrors} from '@angular/forms';
 import { Observable } from 'rxjs';
 import { startWith, map } from 'rxjs/operators';
+import { AbstractControl } from '@angular/forms'
+import { MatSlider } from '@angular/material/slider';
 
 interface Choice {
   value: string;
   viewValue: string;
 }
 
+interface ValidatorFn {
+  (c: AbstractControl) : ValidationErrors | null
+}
 
 @Component({
   selector: 'app-root',
@@ -56,7 +61,6 @@ export class AppComponent implements OnInit {
     {value: 'stat-1', viewValue: 'De stat'}
   ];
 
-
   constructor(private _formBuilder: FormBuilder) {}
 
 
@@ -75,7 +79,6 @@ export class AppComponent implements OnInit {
       medicControl: ['', Validators.required],
     })
    
-
 
     this.filteredOptions = this.firstFormGroup.controls.bodyPartControl.valueChanges
       .pipe(
@@ -128,15 +131,37 @@ export class AppComponent implements OnInit {
   {
     this.json.simptome[0].ultimaDataControl = this.firstFormGroup.controls.lastDateControl.value;
     //console.log( this.json.simptome[0].ultimaDataControl);
-
   }
+
+  getIfCurrentAddress() {
+    this.json.informatii[1].aceeasiAdresa = this.thirdFormGroup.controls.locationControl.value;
+  }
+
+  getDoctorGender() {
+    this.json.informatii[1].genMedic = this.thirdFormGroup.controls.newLocationControl.value;
+  }
+
+  getClinicType() {
+    this.json.informatii[1].tipClinica = this.thirdFormGroup.controls.medicControl.value;
+  }
+
+  onInputChange(event: MatSlider) {
+    console.log("This is emitted as the thumb slides");
+    console.log(event.value);
+  }
+
   viewJson()
   { //console.log("apelez functia")
     console.log( this.json.simptome[0].zonaCorp)
     console.log(this.json.simptome[0].tipSimptom)
     console.log( this.json.simptome[0].bolnav);
     console.log( this.json.simptome[0].ultimaDataControl);
-
+    this.getClinicType()
+    this.getIfCurrentAddress()
+    this.getDoctorGender()
+    console.log( this.json.informatii[1].aceeasiAdresa)
+    console.log(this.json.informatii[1].genMedic) 
+    console.log(this.json.informatii[1].tipClinica) 
   }
   
   formatLabel(value: number) {
